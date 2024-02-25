8575ce7cc8446a86bcf785535dd703c4cdf4ffb6
Syamil <madlim9809@gmail.com>
2024-02-25 16:31:31 +0800

    BST Java commit

---
+++ Main.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        tree.insert(new Node(5));
+        tree.insert(new Node(1));
+        tree.insert(new Node(9));
+        tree.insert(new Node(2));
+        tree.insert(new Node(7));
+        tree.insert(new Node(3));
+        tree.insert(new Node(6));
+        tree.insert(new Node(4));
+        tree.insert(new Node(8));
+
+        tree.display();
+        tree.remove(1);
+        System.out.println(tree.search(11));
+        System.out.println(tree.toString());
+
+    }
+}
